e05a84a63a74dab186a02f00b03a8b8442a65ade
Dinesh Ramasamy <dinesh.vgp@gmail.com>
2017-11-01 15:22:06 +0530

    [changes]: Logo, job desc

--- src/MailTemplate.js
+++ src/MailTemplate.js
@@ -5,13 +5,27 @@ const {Table, TBody, TR, TD} = Oy;
 
 const container = {
   width: '575px',
-  margin: '0 auto'
+  margin: '0 auto',
+  border: '1px solid #E4E7E9',
+  padding: '20px 50px',
+  borderRadius: '0 0 4px 4px',
+  borderTop: 'none'
 };
+
+const containerTop = {
+  width: '573px',
+  margin: '0px auto',
+  border: '1px solid #E4E7E9',
+  borderRadius: '4px 4px 0 0',
+  height: '24px',
+  position: 'relative',
+  backgroundColor: '#F1F4F7'
+}
 
 const headerStyle = {
   fontSize: '60px',
   textAlign: 'center',
-  background: '-webkit-linear-gradient(right,#E58837,#D23A21)',
+  background: '-webkit-linear-gradient(right, #3dca72, #228004)',
   WebkitBackgroundClip: 'text',
   WebkitTextFillColor: 'transparent',
   fontWeight: 'bold'
@@ -27,8 +41,9 @@ const listHash = {
 };
 
 const link = {
-  color: '#d94c19',
-  textDecoration: 'none'
+  color: '#228004',
+  textDecoration: 'none',
+  fontWeight: '400'
 };
 
 const tableMarginTopStyle = {
@@ -85,43 +100,55 @@ const noMarginTopStyle = {
 const data = [{
   id: '1',
   title: 'Operations Engineer - Amazing Opportunity',
-  link: 'http://www.redguava.com.au/jobs/operations_engineer/'
+  link: 'http://www.redguava.com.au/jobs/operations_engineer/',
+  company: 'Red Guava'
 }, {
   id: '2',
   title: 'Tor Implementation Engineer @ Brave Software',
-  link: 'https://www.wfh.io/jobs/3907-tor-implementation-engineer-brave-software'
+  link: 'https://www.wfh.io/jobs/3907-tor-implementation-engineer-brave-software',
+  company: 'Brave Software'
 }, {
   id: 3,
   title: 'Senior Backend Node.JS Engineer @ Formidable',
-  link: 'https://www.wfh.io/jobs/3905-senior-backend-node-js-engineer-formidable'
+  link: 'https://www.wfh.io/jobs/3905-senior-backend-node-js-engineer-formidable',
+  company: 'Formidable'
 }, {
   id: 4,
   title: 'Engineering Manager, JavaScript Team @ Mozilla',
-  link: 'https://www.wfh.io/jobs/3900-engineering-manager-javascript-team-mozilla'
+  link: 'https://www.wfh.io/jobs/3900-engineering-manager-javascript-team-mozilla',
+  company: 'Mozilla'
 }, {
   id: 5,
   title: 'Entrepreneurial Sales Development Hacker in Utah',
-  link: 'https://weworkremotely.com/jobs/5451-entrepreneurial-sales-development-hacker-in'
+  link: 'https://weworkremotely.com/jobs/5451-entrepreneurial-sales-development-hacker-in',
+  company: 'Mokriya'
 }, {
   id: 6,
-  title: 'WANTED: Social Media Manager With Creative Eye + Superb Organisational Skills',
-  link: 'https://weworkremotely.com/jobs/5445-wanted-social-media-manager-with'
+  title: 'WordPress Support & Happiness Engineer',
+  link: 'https://weworkremotely.com/jobs/5638-wordpress-support-happiness-engineer',
+  company: 'CafeMedia'
 }, {
   id: 7,
   title: 'Masterarbeit "Migration bestehender Applikation in eine Serverless Architektur"',
-  link: 'https://stackoverflow.com/jobs/156248/masterarbeit-migration-bestehender-applikation-senacor-technologies-ag?a=QoQpWG6L1OU'
+  link: 'https://stackoverflow.com/jobs/156248/masterarbeit-migration-bestehender-applikation-senacor-technologies-ag?a=QoQpWG6L1OU',
+  company: 'Senacor Technologies AG'
 }, {
   id: 8,
   title: 'Abschlussarbeit zu Smart Contract-Plattformen in der Praxis',
-  link: 'https://stackoverflow.com/jobs/156245/abschlussarbeit-zu-smart-contract-plattformen-in-senacor-technologies-ag'
+  link: 'https://stackoverflow.com/jobs/156245/abschlussarbeit-zu-smart-contract-plattformen-in-senacor-technologies-ag',
+  company: 'Senacor Technologies AG'
 }, {
   id: 9,
   title: 'Frontend developer to Swedish start-up in Connected Health',
-  link: 'https://stackoverflow.com/jobs/155929/frontend-developer-to-swedish-start-up-in-cross-technology-solutions'
+  link: 'https://stackoverflow.com/jobs/155929/frontend-developer-to-swedish-start-up-in-cross-technology-solutions',
+  company: 'Cross Technology Solutions'
 }, {
   id: 10,
   title: 'UI Engineering Manager- Leading Open Source Company',
-  link: 'https://stackoverflow.com/jobs/156126/ui-engineering-manager-leading-open-source-elastic'
+  link: 'https://stackoverflow.com/jobs/156126/ui-engineering-manager-leading-open-source-elastic',
+  company: 'Elastic',
+  location: 'Anywhere',
+  Salary: '$85k - 200k'
 }]
 
 class MailTemplate extends Component {
@@ -181,7 +208,7 @@ class MailTemplate extends Component {
             <TD>
               <a style={jobTitleStyle} href={job.link} target='_blank'>{job.title}</a>
               <div>
-                <a style={jobMetaDataStyle} href="https://news.ycombinator.com/item?id=15368353">techcrunch.com | points: 500 | comments: 255</a>
+                <a style={jobMetaDataStyle} href="https://news.ycombinator.com/item?id=15368353">{job.company} | points: 500 | comments: 255</a>
               </div>
             </TD>
           </TR>
@@ -239,8 +266,35 @@ class MailTemplate extends Component {
   }
 
   render() {
+    const circle = {
+      width: '10px',
+      height: '10px',
+      WebkitBorderRadius: '25px',
+      MozBorderRadius: '25px',
+      borderRadius: '25px',
+      backgroundColor: '#E5E9EC',
+      marginLeft: '5px',
+      top: '7px',
+      position: 'absolute'
+    }
+
+    const circle2 = {
+      ...circle,
+
+      marginLeft: '20px'
+    }
+    const circle3 = {
+      ...circle,
+      marginLeft: '35px'
+    }
     return(
-      <Table style={container}>
+      [
+        <div style={containerTop}>
+          <div style={circle}></div>
+          <div style={circle2}></div>
+          <div style={circle3}></div>
+        </div>,
+        <Table style={container}>
         <TBody>
           <TR>
             <TD>
@@ -252,7 +306,7 @@ class MailTemplate extends Component {
           </TR>
         </TBody>
       </Table>
-    )
+]    )
   }
 
 }
